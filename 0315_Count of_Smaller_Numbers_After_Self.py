7a20a7050e0ae7202dd3f81b1bc1ca0f8b4c0578
Hongfang Lu <luhf09@gmail.com>
2020-09-03 12:00:17 -0400

    Update 0315_Count of_Smaller_Numbers_After_Self.py

--- 0315_Count of_Smaller_Numbers_After_Self.py
+++ 0315_Count of_Smaller_Numbers_After_Self.py
@@ -20,3 +20,53 @@ class Solution:
             #print(right)
         
         return res[::-1]
+
+    
+
+'''
+self-implemented binary search
+'''
+class Solution:
+    def countSmaller(self, nums: List[int]) -> List[int]:
+        res = []
+        
+        right = []
+        
+        def search(li, num):
+            if len(li) == 0:
+                return 0
+            
+            left = 0
+            right = len(li) - 1
+            
+            while left <= right:
+                med = (left + right) // 2
+                
+                if num <= li[med]:
+                    if med == 0:
+                        #print('here')
+                        return med
+                    else:
+                        right = med - 1
+                else:
+                    if med == len(li) - 1:
+                        #print('heereeeee')
+                        return med + 1
+                    elif li[med + 1] >= num:
+                        return med + 1
+                    else:
+                        left = med + 1
+        
+        for i in range(len(nums) - 1, -1, -1):
+            #print(right)
+            num = nums[i]
+            #print(num)
+            #loc = bisect.bisect_left(right, num)
+            loc1 = search(right, num)
+            #print(loc)
+            #print(loc1)
+            res.append(loc1)
+            right.insert(loc1, num)
+            #print(right)
+        
+        return res[::-1]
